4cf7d359fbf4fc358a795413ce8be24c412a274e
GalaxyForest <92831709+Galaxy-Forest@users.noreply.github.com>
2022-05-06 17:24:55 +0800

    Add files via upload

---
+++ js/dazi.js
@@ -0,0 +1,29 @@
+var daZi1 = "从来没有真正的绝境，只有心灵的迷途";
+
+
+
+//动画
+var div1 = document.getElementById("div1");
+
+
+var DHTX = document.getElementById("DHTX");
+
+
+div1.onmouseover = function(){
+   
+    var tou = 0;
+    var wei;
+    // DHTX.innerText=daZi2.slice(0);
+    var daZi2 = "Never really desperate,Only the lost of the soul"
+   daZi2.forEach(function(a) {
+       alert(a)
+   });
+
+   
+
+ 
+   
+
+    
+
+}
